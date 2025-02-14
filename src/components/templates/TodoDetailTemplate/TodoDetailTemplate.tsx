210812dbb5dae91d8725ef5daac8f5bb8ad41cbb
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-15 01:38:23 +0900

    TodoDetailTemplate

---
+++ src/components/templates/TodoDetailTemplate/TodoDetailTemplate.tsx
@@ -0,0 +1,26 @@
+import { useParams } from "react-router";
+import { useTodoContext } from "../../../hooks/useTodoContext";
+import { BaseLayout } from "../../organisms";
+import { InputForm, TextArea } from "../../atoms";
+import styles from "./style.module.css";
+
+export const TodoDetailTemplate = () => {
+  const { originTodoList } = useTodoContext();
+  const { id } = useParams();
+  const todo = originTodoList.find((todo) => String(todo.id) === id);
+
+  return (
+    <BaseLayout title={"TodoDetail"}>
+      {!!todo && (
+        <div className={styles.container}>
+          <div className={styles.area}>
+            <InputForm disabled value={todo.title} placeholder={"Title"} />
+          </div>
+          <div className={styles.area}>
+            <TextArea disabled value={todo.content} placeholder={"Content"} />
+          </div>
+        </div>
+      )}
+    </BaseLayout>
+  );
+};
