39805f63c1d7d0fab390adbc8d46a776c3a6176f
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-15 01:50:54 +0900

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { TodoProvider } from "./contexts/TodoContext";
+import { Router } from "./router";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div></div>
+    <TodoProvider>
+      <Router />
+    </TodoProvider>
   </StrictMode>
 );
