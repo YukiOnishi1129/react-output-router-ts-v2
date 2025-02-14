ce75265ab7c1f36b49a46bcbfe0488a248615e9b
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-15 01:20:40 +0900

    organisms

---
+++ src/components/organisms/BaseLayout/BaseLayout.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from "react";
+
+import { Navigation } from "../../molecules/Navigation";
+import styles from "./style.module.css";
+
+type BaseLayoutProps = {
+  children: ReactNode;
+  title: string;
+};
+
+export const BaseLayout: FC<BaseLayoutProps> = ({ children, title }) => (
+  <div className={styles.container}>
+    <section className={styles.common}>
+      <Navigation />
+    </section>
+    <h1 className={styles.title}>{title}</h1>
+    {children}
+  </div>
+);
